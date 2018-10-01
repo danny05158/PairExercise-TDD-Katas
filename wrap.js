4c5cffc3b9774823a5e0c866a38acab533ec9ea0
Paul Ebreo <paulebreo@triton.edu>
2018-10-01 10:31:31 -0500

    progress on wrap logic

--- wrap.js
+++ wrap.js
@@ -1,3 +1,20 @@
-const wrap = () => {}
+const wrap = (str, num) => {
+  let currentPos = 0
+  let result = []
+  for (let i = 0; i < str.length; i++) {
+    if (i === num) {
+      let currentVal = str[i]
+      // is it blank?
+      if (currentVal === " ") {
+        let subStr = str.slice(0, i)
+        result.push(subStr)
+      }
+      // it is not blank
+    }
+  }
+  return result.join("\n")
+}
+let r = wrap("hi there", 4)
+console.log(r)
 
 module.exports = wrap
--- wrap.spec.js
+++ wrap.spec.js
@@ -1,23 +1,23 @@
-const { expect } = require('chai');
-const wrap = require('./wrap');
+const { expect } = require("chai")
+const wrap = require("./wrap")
 
-describe('Wrap', function() {
-  it('should be a function', function() {
-    expect(typeof wrap).to.equal('function');
-  });
-  xit('should take two arguments', function() {
-    expect(wrap.arguments.length.to.equal(2));
-  });
-  it('should return a string', function() {
-    let returnVal = wrap();
-    expect(typeof returnVal).to.equal('string');
-  });
-  it('should break every n characters', function() {
-    let returnVal = wrap('Hi there', 3);
-    expect(returnVal).to.equal('Hi\nthere');
-  });
-  it('should break every n characters and account for imperfect scenario', function() {
-    let returnVal = wrap('Hi there', 4);
-    expect(returnVal).to.equal('Hi\nthere');
-  });
-});
+describe("Wrap", function() {
+  it("should be a function", function() {
+    expect(typeof wrap).to.equal("function")
+  })
+  xit("should take two arguments", function() {
+    expect(wrap.arguments.length.to.equal(2))
+  })
+  it("should return a string", function() {
+    let returnVal = wrap()
+    expect(typeof returnVal).to.equal("string")
+  })
+  it("should break every n characters", function() {
+    let returnVal = wrap("Hi there", 3)
+    expect(returnVal).to.equal("Hi\nthere")
+  })
+  it("should break every n characters and account for imperfect scenario", function() {
+    let returnVal = wrap("Hi there", 4)
+    expect(returnVal).to.equal("Hi\nthere")
+  })
+})
