13edbf883fe846c5c0f5bf0948e7b05bd99c7036
Daniel Casasola <casasolad86@gmail.com>
2018-10-01 10:19:24 -0500

    inputed multiple it statements for test

--- wrap.spec.js
+++ wrap.spec.js
@@ -1,13 +1,23 @@
-const { expect } = require("chai")
-const wrap = require("./wrap")
+const { expect } = require('chai');
+const wrap = require('./wrap');
 
-describe("Wrap", function() {
-  it("should be a function", function() {
-    console.log("wrap", wrap)
-    expect(typeof wrap).to.equal("function")
-  })
-  it("should take two arguments", function() {})
-  it("should return a string", function() {})
-  it("should break every n characters", function() {})
-  it("should break at the column number provided", function() {})
-})
+describe('Wrap', function() {
+  it('should be a function', function() {
+    expect(typeof wrap).to.equal('function');
+  });
+  xit('should take two arguments', function() {
+    expect(wrap.arguments.length.to.equal(2));
+  });
+  it('should return a string', function() {
+    let returnVal = wrap();
+    expect(typeof returnVal).to.equal('string');
+  });
+  it('should break every n characters', function() {
+    let returnVal = wrap('Hi there', 3);
+    expect(returnVal).to.equal('Hi\nthere');
+  });
+  it('should break every n characters and account for imperfect scenario', function() {
+    let returnVal = wrap('Hi there', 4);
+    expect(returnVal).to.equal('Hi\nthere');
+  });
+});
